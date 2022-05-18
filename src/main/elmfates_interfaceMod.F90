6ec691834371b364e50007900bd9c0ba4e032dc7
YanlanLiu <liuyanlan10@gmail.com>
2022-05-17 20:37:34 -0700

    assuming equal area for all sites in one grid, distribute seed_id_global for each site

--- src/main/elmfates_interfaceMod.F90
+++ src/main/elmfates_interfaceMod.F90
@@ -2150,7 +2150,8 @@ contains
        ! loop over pft. Disperse seeds for pft = 9 for now
        write(iulog,*) 's, c, g, seed_id_global(g): ', s, c, g, seed_id_global(g)
        write(iulog,*) 'BEFORE, this%fates(nc)%bc_in(s)%seed_in(9), this%fates(nc)%bc_out(s)%seed_out(9): ', this%fates(nc)%bc_in(s)%seed_in(9), this%fates(nc)%bc_out(s)%seed_out(9)
-       this%fates(nc)%bc_in(s)%seed_in(9) = seed_id_global(g)
+       this%fates(nc)%bc_in(s)%seed_in(9) = seed_id_global(g)/this%fates(nc)%nsites ! assuming equal area for all sites, seed_id_global in [kg/grid/day], seed_in in [kg/site/day]
+
        this%fates(nc)%bc_out(s)%seed_out(9) = 0._r8 ! reset seed_out
 
        write(iulog,*) 'AFTER, this%fates(nc)%bc_in(s)%seed_in(9), this%fates(nc)%bc_out(s)%seed_out(9): ', this%fates(nc)%bc_in(s)%seed_in(9), this%fates(nc)%bc_out(s)%seed_out(9)
